6312397f6c13d32db53db3294a18d19b19baa8fa
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-28 08:06:41 +0200

    WIP fix gitsigns, move to default

--- lua/user/plugins/git/gitsigns.lua
+++ lua/user/plugins/git/gitsigns.lua
@@ -14,81 +14,83 @@ return {
 
         config = function()
             local gitsigns = require('gitsigns')
-            gitsigns.setup({
-                signs = {
-                    add = {
-                        hl = 'GitSignsAdd',
-                        text = icons.ui.BoldLineLeft,
-                        numhl = 'GitSignsAddNr',
-                        linehl = 'GitSignsAddLn',
-                    },
-                    change = {
-                        hl = 'GitSignsChange',
-                        text = icons.ui.BoldLineLeft,
-                        numhl = 'GitSignsChangeNr',
-                        linehl = 'GitSignsChangeLn',
-                    },
-                    delete = {
-                        hl = 'GitSignsDelete',
-                        text = icons.ui.Triangle,
-                        numhl = 'GitSignsDeleteNr',
-                        linehl = 'GitSignsDeleteLn',
-                    },
-                    topdelete = {
-                        hl = 'GitSignsDelete',
-                        text = icons.ui.Triangle,
-                        numhl = 'GitSignsDeleteNr',
-                        linehl = 'GitSignsDeleteLn',
-                    },
-                    changedelete = {
-                        hl = 'GitSignsChange',
-                        text = icons.ui.BoldLineLeft,
-                        numhl = 'GitSignsChangeNr',
-                        linehl = 'GitSignsChangeLn',
-                    },
-                },
-                signcolumn = true,
-                numhl = false,
-                linehl = false,
-                word_diff = false,
-                watch_gitdir = {
-                    interval = 1000,
-                    follow_files = true,
-                },
-                attach_to_untracked = true,
-                -- git blame
-                -- current_line_blame = vim.g.enable_gitblame, -- Toggle with `:Gitsigns toggle_current_line_blame`
-                current_line_blame = _G.user.enable_gitblame, -- Toggle with `:Gitsigns toggle_current_line_blame`
-                current_line_blame_opts = {
-                    virt_text = true,
-                    virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
-                    delay = 2000,
-                    ignore_whitespace = true,
-                },
-
-                current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-                sign_priority = 6,
-                status_formatter = nil, -- Use default
-                update_debounce = 200,
-                max_file_length = 40000,
-                preview_config = {
-                    -- Options passed to nvim_open_win
-                    border = 'rounded',
-                    style = 'minimal',
-                    relative = 'cursor',
-                    row = 0,
-                    col = 1,
-                },
-                yadm = { enable = false },
-
-                -- signs = {
-                --     add = { text = '+' },
-                --     change = { text = '~' },
-                --     delete = { text = '_' },
-                --     topdelete = { text = '‾' },
-                --     changedelete = { text = '~' },
-                -- },
-            })
+            gitsigns.setup()
+            -- gitsigns.setup({
+            --     signs = {
+            --         add = {
+            --             -- hl = 'GitSignsAdd',
+            --             text = icons.ui.BoldLineLeft,
+            --             -- numhl = 'GitSignsAddNr',
+            --             -- linehl = 'GitSignsAddLn',
+            --         },
+            --         change = {
+            --             -- hl = 'GitSignsChange',
+            --             text = icons.ui.BoldLineLeft,
+            --             -- numhl = 'GitSignsChangeNr',
+            --             -- linehl = 'GitSignsChangeLn',
+            --         },
+            --         delete = {
+            --             -- hl = 'GitSignsDelete',
+            --             text = icons.ui.Triangle,
+            --             -- numhl = 'GitSignsDeleteNr',
+            --             -- linehl = 'GitSignsDeleteLn',
+            --         },
+            --         topdelete = {
+            --             -- hl = 'GitSignsDelete',
+            --             text = icons.ui.Triangle,
+            --             -- numhl = 'GitSignsDeleteNr',
+            --             -- linehl = 'GitSignsDeleteLn',
+            --         },
+            --         changedelete = {
+            --             -- hl = 'GitSignsChange',
+            --             text = icons.ui.BoldLineLeft,
+            --             -- numhl = 'GitSignsChangeNr',
+            --             -- linehl = 'GitSignsChangeLn',
+            --         },
+            --     },
+            --     signcolumn = true,
+            --     numhl = true,
+            --     linehl = false,
+            --     word_diff = false,
+            --     watch_gitdir = {
+            --         interval = 1000,
+            --         follow_files = true,
+            --     },
+            --     auto_attach = true,
+            --     attach_to_untracked = false, -- changed
+            --     -- git blame
+            --     -- current_line_blame = vim.g.enable_gitblame, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            --     current_line_blame = _G.user.enable_gitblame, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            --     current_line_blame_opts = {
+            --         virt_text = true,
+            --         virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+            --         delay = 2000,
+            --         ignore_whitespace = true,
+            --     },
+            --
+            --     current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+            --     sign_priority = 6,
+            --     status_formatter = nil, -- Use default
+            --     update_debounce = 200,
+            --     max_file_length = 40000,
+            --     preview_config = {
+            --         -- Options passed to nvim_open_win
+            --         border = 'rounded',
+            --         style = 'minimal',
+            --         relative = 'cursor',
+            --         row = 0,
+            --         col = 1,
+            --     },
+            --     -- yadm = { enable = false },
+            --
+            --     -- signs = {
+            --     --     add = { text = '+' },
+            --     --     change = { text = '~' },
+            --     --     delete = { text = '_' },
+            --     --     topdelete = { text = '‾' },
+            --     --     changedelete = { text = '~' },
+            --     -- },
+            -- })
 
             vim.keymap.set(
                 'n',
