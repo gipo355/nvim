777a497674eb81f523c4e6b48ea8cdfdff3fda72
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-30 22:34:51 +0200

    WIP snyk no erro

--- lua/user/plugins/lsp/lspconfig/configs/server-configs/snyk_ls.lua
+++ lua/user/plugins/lsp/lspconfig/configs/server-configs/snyk_ls.lua
@@ -4,7 +4,7 @@ M.setup = function()
     if not ok then
         -- vim.print('Error loading user.env, snyk_ls will not work properly.')
         -- print error message to user
-        print('Error loading user.env, snyk_ls will not work properly.')
+        -- print('Error loading user.env, snyk_ls will not work properly.')
         -- throw an error to stop the execution of the function
         -- error('Error loading user.env, snyk_ls will not work properly.')
         -- throw a warning instead
