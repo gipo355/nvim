0ec74e9eaee9416478d0a13516c0cd996655bd18
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-07-09 07:07:04 +0200

    WIP add tab fn

--- lua/user/plugins/singles/nvim-tree/utils/tabs_keybinds_opts.lua
+++ lua/user/plugins/singles/nvim-tree/utils/tabs_keybinds_opts.lua
@@ -1,16 +1,36 @@
 local set_desc = require('user.utils.functions').set_keymap_desc
 
+local function changeTabOrCreate(cmd)
+    return function()
+        -- Get the number of open tabs
+        local tab_count = vim.fn.tabpagenr('$')
+
+        -- Check if there is an open tab
+        if tab_count > 1 then
+            vim.cmd(cmd)
+        else
+            vim.cmd('tabnew')
+        end
+    end
+end
+
 -- ##### USING TABS, conflicting with c-i? #####
 -- vim.keymap.set('n', '<C-i>', '<C-i>', set_desc('Tab conflicting')) -- should prevent c-i   from being mapped to tab, doesn't work with tmux
 
 -- tabs keymaps
 -- keymap("n", "<tab>", "<cmd>tabnext<cr>", opts)
 -- vim.keymap.set('n', '<s-tab>', '<cmd>tabprev<cr>', set_desc('Previous tab'))
-vim.keymap.set('n', '!', '<cmd>tabprev<cr>', set_desc('Previous tab'))
+
+-- vim.keymap.set('n', '!', '<cmd>tabprev<cr>', set_desc('Previous tab'))
+vim.keymap.set('n', '!', changeTabOrCreate('tabprev'), set_desc('Previous tab'))
+
 -- vim.keymap.set('n', '<M-tab>', '<cmd>tabnext<cr>', set_desc('Next tab'))
 -- vim.keymap.set('n', '<tab>', '<cmd>tabnext<cr>', set_desc('Next tab')) -- disables c-i
 -- vim.keymap.set('n', '<C-tab>', '<cmd>tabnext<cr>', set_desc('Next tab'))
-vim.keymap.set('n', '_', '<cmd>tabnext<cr>', set_desc('Next tab'))
+
+-- vim.keymap.set('n', '_', '<cmd>tabnext<cr>', set_desc('Next tab'))
+vim.keymap.set('n', '_', changeTabOrCreate('tabnext'), set_desc('Next tab'))
+
 vim.keymap.set('n', '<C-w><tab>', '<cmd>tabnew<cr>', set_desc('New tab'))
 vim.keymap.set('n', '<C-w><S-tab>', '<cmd>tabclose<cr>', set_desc('Close tab'))
 -- leader tab keymaps
