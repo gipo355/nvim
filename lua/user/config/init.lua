432b2c4fb5aa18ab23fc58b95aca06824d5812bc
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-08-11 10:40:16 +0200

    aerial comment

--- lua/user/config/init.lua
+++ lua/user/config/init.lua
@@ -128,7 +128,7 @@ _G.user.mini_map = {
 -- alternative minimap and scrollbar, satellite (good with arrow.nvim)
 -- lighter than mini map
 _G.user.enable_satellite_scrollbar = false
--- there is another option: aria.nvim, symbols-outline.nvim, lspsaga symbols-outline, trouble symbols
+-- there is another option: aerial.nvim, symbols-outline.nvim, lspsaga symbols-outline, trouble symbols
 -- they can all function as symbols minimap
 
 --------------------------------
