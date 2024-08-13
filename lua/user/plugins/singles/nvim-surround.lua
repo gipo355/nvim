e1a9aaf00cfc14016b86ae3dc96eeab08eb002e2
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-08-13 16:07:19 +0200

    WIP surround

--- lua/user/plugins/singles/nvim-surround.lua
+++ lua/user/plugins/singles/nvim-surround.lua
@@ -1,5 +1,15 @@
 -- # surround, conflicts with mini.surround
 -- also conflicts with flash.nvim (ds is jump)
+-- example: replace with own keybinds
+--     Old text                    Command         New text
+--------------------------------------------------------------------------------
+-- surr*ound_words             ysiw)           (surround_words)
+-- *make strings               ys$"            "make strings"
+-- [delete ar*ound me!]        ds]             delete around me!
+-- remove <b>HTML t*ags</b>    dst             remove HTML tags
+-- 'change quot*es'            cs'"            "change quotes"
+-- <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+-- delete(functi*on calls)     dsf             function calls
 return {
     'kylechui/nvim-surround',
     event = 'VeryLazy',
@@ -14,9 +24,9 @@ return {
                 normal_cur = 'cvass',
                 normal_line = 'cvaS',
                 normal_cur_line = 'cvaSS',
-                visual = 'cvv',
-                visual_line = 'cvs',
-                delete = 'cvds',
+                visual = 'cvj',
+                visual_line = 'cvl',
+                delete = 'cvd',
                 change = 'cvrs',
                 change_line = 'cvrS',
             },
