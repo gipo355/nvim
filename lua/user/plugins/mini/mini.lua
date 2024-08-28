4b81004ab21b47c7c5a0c36aae893e8d0336556a
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-08-28 09:39:13 +0200

    WIP mini comment

--- lua/user/plugins/mini/mini.lua
+++ lua/user/plugins/mini/mini.lua
@@ -266,7 +266,24 @@ return {
             --  - ci'  - [C]hange [I]nside [']quote
             require('mini.ai').setup({ n_lines = 500 })
 
-            require('mini.comment').setup()
+            require('mini.comment').setup({
+                -- Module mappings. Use `''` (empty string) to disable one.
+                mappings = {
+                    -- Toggle comment (like `gcip` - comment inner paragraph) for both
+                    -- Normal and Visual modes
+                    comment = '<leader>/',
+
+                    -- Toggle comment on current line
+                    comment_line = '<leader>/',
+
+                    -- Toggle comment on visual selection
+                    comment_visual = '<leader>/',
+
+                    -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+                    -- Works also in Visual mode if mapping differs from `comment_visual`
+                    textobject = '<leader>/',
+                },
+            })
 
             require('mini.fuzzy').setup()
 
