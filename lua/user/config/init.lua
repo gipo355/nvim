6d9d6ac5ef6f52a4e2724ce21d0793d1898fa9de
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-12-17 21:53:24 +0100

    theme

--- lua/user/config/init.lua
+++ lua/user/config/init.lua
@@ -409,9 +409,9 @@ _G.user.theme = function()
     -- my favorites
 
     -- return 'vague'
-    return 'rose-pine'
+    -- return 'rose-pine'
     -- return 'monokai-pro'
-    -- return 'gruvbox-baby'
+    return 'gruvbox-baby'
     -- return 'kanagawa'
     -- return 'gruvbox-material'
     -- return 'ares'
