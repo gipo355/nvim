8a030de3b1171fa8a36d5a93e55c8382ab3f5dac
gipo355 <91525512+gipo355@users.noreply.github.com>
2025-01-06 21:40:14 +0100

    WIP blink source codecompanion

--- lua/user/plugins/cmp/blink.lua
+++ lua/user/plugins/cmp/blink.lua
@@ -47,7 +47,7 @@ return {
         -- default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, via `opts_extend`
         sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
+            default = { 'lsp', 'path', 'snippets', 'buffer', 'codecompanion' },
             -- optionally disable cmdline completions
             -- cmdline = {},
             cmdline = function()
