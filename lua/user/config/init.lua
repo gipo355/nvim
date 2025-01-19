a66c4bf5a437b6429c26cd9a570b663646fcaa15
gipo355 <91525512+gipo355@users.noreply.github.com>
2025-01-19 15:09:28 +0100

    WIP neovide, vstls

--- lua/user/config/init.lua
+++ lua/user/config/init.lua
@@ -251,7 +251,7 @@ _G.user.lsp = {
     -- enable format on save
     format_on_save = false,
     ---@type "typescript-tools" | "deno" | "coc" | "vtsls" -- WARN: coc is broken
-    tsserver = 'typescript-tools',
+    tsserver = 'vtsls',
     -- this is a list of lsp formatters passed to a filter function
     -- in conform to prevent autoformatting on save from those sources
     skipped_lsp_formatters = {
