9c6e559a01a6162476a34aedbf26d8baf8d0f467
gipo355 <91525512+gipo355@users.noreply.github.com>
2025-03-22 10:11:02 +0100

    WIP conform format fn

--- lua/user/plugins/lsp/formatters/conform.lua
+++ lua/user/plugins/lsp/formatters/conform.lua
@@ -329,7 +329,7 @@ return {
 
             vim.keymap.set(
                 'n',
-                '<leader>lF',
+                '<leader>lf',
                 format_function,
                 set_desc('format')
             )
