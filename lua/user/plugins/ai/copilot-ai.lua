ef999ebc8bfddb2f1b945ba72ee21d4e9c4343b2
gipo355 <91525512+gipo355@users.noreply.github.com>
2025-04-01 22:06:53 +0200

    WIP remove gitdiff from copilot ai

--- lua/user/plugins/ai/copilot-ai.lua
+++ lua/user/plugins/ai/copilot-ai.lua
@@ -70,9 +70,9 @@ Follow commitlint rules. Add breaking change note only if necessary.
                         -- Start with a short sentence in imperative form, no more than 50 characters long.
                         -- Then leave an empty line and continue with a more detailed explanation.
                         -- Write only one sentence for the first part, and two or three sentences at most for the detailed explanation.
-                        selection = function(source)
-                            return select.gitdiff(source, true)
-                        end,
+                        -- selection = function(source)
+                        --     return select.gitdiff(source, true)
+                        -- end,
                     },
                 },
             })
